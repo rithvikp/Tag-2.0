2ba4ec233e86e5a97cd9a746b76e8ef4e6a765d1
RithvikP <RithvikP@users.noreply.github.com>
2017-05-26 19:26:29 -0700

    Initial Commit

---
+++ Tag2.0/src/main/SimplePlayer.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.awt.Color;
+
+public class SimplePlayer extends Player{
+
+	public SimplePlayer(int ix, int iy, int ispeed, Color icolor){
+		super(ix,iy,ispeed,new int[0],icolor);
+	}
+	
+	public void move()
+	{
+		
+	}
+	
+	
+
+}
